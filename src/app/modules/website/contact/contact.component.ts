e4186f31c8fa07024c77b2ba9ed4a7bea2b5e453
Amrit <amritpalsharma.cts@gmail.com>
2025-04-03 13:00:23 +0530

    working on tickets 3 April 2025

--- src/app/modules/website/contact/contact.component.ts
+++ src/app/modules/website/contact/contact.component.ts
@@ -4,6 +4,9 @@ import { HttpClient } from '@angular/common/http';
 import { WebPages } from '../../../services/webpages.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from '../../../../environments/environment';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -11,9 +14,15 @@ import { environment } from '../../../../environments/environment';
 })
 
 export class ContactComponent implements OnInit {
-  apiUrl:any = environment.url;
-  disableSentButton:boolean=false;
+  apiUrl: any = environment.url;
+  disableSentButton: boolean = false;
   base_url: string = '';
+  errorTxt: string = '';
+  successTxt: string = '';
+  phoneRequired: string = '';
+  invalidPhoneNumber: string = '';
+  requiredFieldsMessage: string = '';
+  provideEmailAddress: string = '';
   address: string = '';
   semail: string = '';
   banner_title: string = '';
@@ -23,11 +32,12 @@ export class ContactComponent implements OnInit {
   submit_btn_txt: string = '';
   talent_label_txt: string = '';
   txt_before_radio_btn: string = '';
-  advertisementList:any;
-  advertisemnet_base_url:string = '';
+  advertisementList: any;
+  advertisemnet_base_url: string = '';
+  messageText:string='';
 
-  isLoading : boolean = true;
-  btnLoading : boolean = true;
+  isLoading: boolean = true;
+  btnLoading: boolean = true;
   countdown: number = 10;
 
 
@@ -39,54 +49,55 @@ export class ContactComponent implements OnInit {
   captchaResolved = false; // Track if captcha is resolved
   recaptchaToken: string | null = null; // Captcha token for backend validation
   responseMessage: string = '';
-  messageType: string = '';  
+  messageType: string = '';
   name_placeholder: string = '';
   email_placeholder: string = '';
   phone_placeholder: string = '';
   message_placeholder: string = '';
   showcaptchaError: boolean = false;
   constructor(
-    private route: ActivatedRoute, 
+    private route: ActivatedRoute,
     private webPages: WebPages,
     private fb: FormBuilder,
     private http: HttpClient,
     private router: Router,
-    ) { }
+    private toastr: ToastrService,
+    private translateService: TranslateService
+  ) { }
 
   ngOnInit(): void {
     // Initialize form with validation rules
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      // phone: ['', [Validators.required]], // Adjust pattern as needed
-      // phone: ['', [Validators.required, Validators.pattern(/^\+?(41|49|39|33|44|34|351|32|45|46)\d{6,12}$/)]],
-      // phone: ['', [Validators.required, Validators.pattern(/^\+?(41|49|39|33|44|34|351|32|45|46)\d{7,10}$/)]],
       phone: ['', [
         Validators.required,
-        Validators.pattern(/^\+?(41|49|39|33|44|34|351|32|45|46)\d{7,10}$/)
+        // Validators.pattern(/^\+?(41|49|39|33|44|34|351|32|45|46)\d{7,}$/)
       ]],
-      message: ['', Validators.required],
-      domain : window.location.hostname,
-      lang : localStorage.getItem('lang_id'),
+      // message: ['', Validators.required],
+      domain: window.location.hostname,
+      lang: localStorage.getItem('lang_id'),
     });
+    this.getToasterMsg();
     this.webPages.languageId$.subscribe((data) => {
       this.getPageData(data)
+      this.getToasterMsg();
     });
   }
 
-  
-  isActive : any ={
+
+  isActive: any = {
     skyscraper: true,
     wide_skyscraper: true,
     leaderboard: true,
-    large_leaderboard:true,
+    large_leaderboard: true,
     banner: true,
-    square:true,
+    square: true,
     small_square: true,
     large_rectangle: true,
     inline_rectangle: true,
   }
-  advertisementData:any = {
+  advertisementData: any = {
     skyscraper: {
       id: '1',
       featured_image: "leaderboard.png"
@@ -125,33 +136,33 @@ export class ContactComponent implements OnInit {
     },
   }
 
-  getPageData(languageId: any){
-    this.webPages.getDynamicContentPage('contact',languageId).subscribe((res) => {
-      if(res.status){
-          this.address = res.data.pageData.address;
-          this.banner_title = res.data.pageData.banner_title;
-          this.club_label_txt = res.data.pageData.club_label_txt;
-          this.form_title = res.data.pageData.form_title;
-          this.scout_label_txt = res.data.pageData.scout_label_txt;
-          this.submit_btn_txt = res.data.pageData.submit_btn_txt;
-          this.talent_label_txt = res.data.pageData.talent_label_txt;
-          this.txt_before_radio_btn = res.data.pageData.txt_before_radio_btn;
-          this.semail= res.data.pageData.email;
-          this.advertisementData =  res.data.advertisementData;
-          this.advertisementList =  res.data.allAdsList;
-          // this.advertisementData = [];
+  getPageData(languageId: any) {
+    this.webPages.getDynamicContentPage('contact', languageId).subscribe((res) => {
+      if (res.status) {
+        this.address = res.data.pageData.address;
+        this.banner_title = res.data.pageData.banner_title;
+        this.club_label_txt = res.data.pageData.club_label_txt;
+        this.form_title = res.data.pageData.form_title;
+        this.scout_label_txt = res.data.pageData.scout_label_txt;
+        this.submit_btn_txt = res.data.pageData.submit_btn_txt;
+        this.talent_label_txt = res.data.pageData.talent_label_txt;
+        this.txt_before_radio_btn = res.data.pageData.txt_before_radio_btn;
+        this.semail = res.data.pageData.email;
+        this.advertisementData = res.data.advertisementData;
+        this.advertisementList = res.data.allAdsList;
+        // this.advertisementData = [];
 
-          
-          this.advertisemnet_base_url = res.data.advertisemnet_base_url;
-          this.base_url =  res.data.base_url;
-          this.name_placeholder = res.data.pageData.name_placeholder;
-          this.phone_placeholder = res.data.pageData.phone_placeholder;
-          this.email_placeholder = res.data.pageData.email_placeholder;
-          this.message_placeholder = res.data.pageData.message_placeholder;
 
-          this.isLoading = false;
-          this.startCountdown();
-        }
+        this.advertisemnet_base_url = res.data.advertisemnet_base_url;
+        this.base_url = res.data.base_url;
+        this.name_placeholder = res.data.pageData.name_placeholder;
+        this.phone_placeholder = res.data.pageData.phone_placeholder;
+        this.email_placeholder = res.data.pageData.email_placeholder;
+        this.message_placeholder = res.data.pageData.message_placeholder;
+
+        this.isLoading = false;
+        this.startCountdown();
+      }
     });
   }
 
@@ -165,42 +176,7 @@ export class ContactComponent implements OnInit {
       }
     }, 1000);
   }
-  // isEmptyObject(obj:any) {
-  //   if(typeof obj != 'undefined'){
-  //     return (obj && (Object.keys(obj).length === 0));
-  //   }
-  //   return true;
-  // }
-  // closeAd(object: any) {
 
-  //   switch(object){
-  //     case 'skyscraper':
-  //         this.advertisementData.skyscraper = [];
-  //         break;
-  //     case 'small_square':
-  //         this.advertisementData.small_square = [];
-  //         break;
-  //     case 'leaderboard':
-  //         this.advertisementData.leaderboard = [];
-  //         break;
-  //     case 'large_leaderboard':
-  //         this.advertisementData.large_leaderboard = [];
-  //         break;
-  //     case 'large_rectangle':
-  //         this.advertisementData.large_rectangle = [];
-  //         break;
-  //     case 'inline_rectangle':
-  //         this.advertisementData.inline_rectangle = [];
-  //         break;
-  //     case 'square':
-  //         this.advertisementData.square = [];
-  //         break;
-  //     default:
-  //         //when no case is matched, this block will be executed;
-  //         break;  //optional
-  //     }
-
-  // }
   // Form field getters for template validation
   get name() {
     return this.contactForm.get('name');
@@ -231,30 +207,58 @@ export class ContactComponent implements OnInit {
     }
   }
 
+
+  showValidationErrors() {
+    Object.keys(this.contactForm.controls).forEach((key) => {
+      const controlErrors = this.contactForm.get(key)?.errors;
+      if (controlErrors) {
+        Object.keys(controlErrors).forEach((errorKey) => {
+          let errorMessage = this.getErrorMessage(key, errorKey);
+          this.toastr.error(errorMessage, this.errorTxt); // Show each error in a toaster
+        });
+      }
+    });
+  }
+
+  getErrorMessage(field: string, errorType: string): string {
+    const errorMessages: { [key: string]: any } = {
+      email: {
+        required: this.provideEmailAddress,
+        email: this.provideEmailAddress,
+      },
+      phone: {
+        required: this.phoneRequired,
+        pattern: this.invalidPhoneNumber,
+      },
+    };
+
+    return errorMessages[field]?.[errorType] || this.requiredFieldsMessage;
+  }
+
   // Handle form submission
-  onSubmit(): void {  
+  onSubmit(): void {
     // return false;
-    let role ;
-    if(this.selectedOption === 'option1'){
+    let role;
+    if (this.selectedOption === 'option1') {
       role = 1;
     }
-    else if(this.selectedOption === 'option2'){
+    else if (this.selectedOption === 'option2') {
       role = 2;
     }
-    else{
+    else {
       role = 3;
     }
-    if(!this.contactForm.valid) {
+    if (!this.contactForm.valid) {
       console.log('toched');
       this.contactForm.markAllAsTouched();
     }
-     if (this.contactForm.valid && this.captchaResolved && this.recaptchaToken) {
+    if (this.contactForm.valid && this.captchaResolved && this.recaptchaToken) {
 
       const formData = { ...this.contactForm.value, captchaToken: this.recaptchaToken };
       const result = {};
       // Send the form data and captcha token to the server
       this.http.post('/api/verify-captcha', formData).subscribe(
-        (response) => { 
+        (response) => {
 
           // console.log('Form submitted successfully:', result);
         },
@@ -263,15 +267,15 @@ export class ContactComponent implements OnInit {
         }
       );
       this.disableSentButton = true;
-      const ContactformData = { ...this.contactForm.value, captchaToken: this.recaptchaToken, role: role };
-      
-      this.http.post<any>(this.apiUrl+'/frontend/save-contact-form', ContactformData).subscribe(
+      const ContactformData = { ...this.contactForm.value, captchaToken: this.recaptchaToken, role: role, message:'this.messageText' };
+
+      this.http.post<any>(this.apiUrl + '/frontend/save-contact-form', ContactformData).subscribe(
         (response) => {
           // console.log('Form submitted successfully:', response);
-          if(response.message != '' && response.data.redirect_url != ''){
+          if (response.message != '' && response.data.redirect_url != '') {
             this.setResponseMessage(response.message, response.data.class);
-            this.router.navigate(['/'+response.data.redirect_url]);
-          }else{
+            this.router.navigate(['/' + response.data.redirect_url]);
+          } else {
             console.log('something went wrong');
           }
           this.disableSentButton = false;
@@ -283,10 +287,11 @@ export class ContactComponent implements OnInit {
 
     } else {
       if (!this.captchaResolved) {
-          this.showcaptchaError = true;
+        this.showcaptchaError = true;
       }
       if (this.contactForm.invalid) {
-        console.error('Form is invalid:', this.contactForm.errors);
+        console.error('Form is invalid:', this.contactForm.invalid);
+        this.showValidationErrors();
       }
     }
   }
@@ -309,8 +314,8 @@ export class ContactComponent implements OnInit {
 
   }
 
-  isEmptyObject(obj:any) {
-    if(typeof obj != 'undefined'){
+  isEmptyObject(obj: any) {
+    if (typeof obj != 'undefined') {
       return (obj && (Object.keys(obj).length === 0));
     }
     return true;
@@ -328,9 +333,6 @@ export class ContactComponent implements OnInit {
     return false;
   }
 
-  // isExists(key: any): boolean {
-  //   return key in this.advertisementData;
-  // }
 
   isExists(key: any): boolean {
     return (this.advertisementData && key in this.advertisementData) || this.advertisementList.includes(key);
@@ -339,18 +341,31 @@ export class ContactComponent implements OnInit {
   isFeaturedImageExists(key: any): boolean {
     return this.advertisementData && this.advertisementData[key] && 'featured_image' in this.advertisementData[key];
   }
-  
+
   allowOnlyNumbers(event: KeyboardEvent) {
     const charCode = event.which ? event.which : event.keyCode;
     if (charCode < 48 || charCode > 57) {
       event.preventDefault(); // Block non-numeric input
     }
   }
-  
+
   preventTextPaste(event: ClipboardEvent) {
     const pasteData = event.clipboardData?.getData('text');
     if (!pasteData || !/^\d+$/.test(pasteData)) {
       event.preventDefault(); // Block paste if it contains non-numeric characters
     }
+  }
+
+  getToasterMsg() {
+    this.translateService.get(['pleaseWait', 'phoneRequired', 'success!', 'error', 'provideEmailAddress', 'invalidPhoneNumber', 'requiredFieldsMessage']).subscribe((translations) => {
+      // this.pleaseWait = translations['pleaseWait'];
+      this.phoneRequired = translations['phoneRequired'];
+      this.provideEmailAddress = translations['provideEmailAddress'];
+      this.invalidPhoneNumber = translations['invalidPhoneNumber'];
+      this.requiredFieldsMessage = translations['requiredFieldsMessage'];
+      // this.uploadingPhotos = translations['uploadingPhotos'];
+      this.successTxt = translations['success!'];
+      this.errorTxt = translations['errorTxt'];
+    });
   }
 }
--- src/app/modules/website/index/index.component.ts
+++ src/app/modules/website/index/index.component.ts
@@ -358,14 +358,19 @@ export class IndexComponent {
         this.hero_bg_img_dark_mode = res.data.base_url + pageData.hero_bg_img_dark_mode;
 
         this.sliderDetail = sliderData;
+        let detailTxt;
         if (sliderData.totalCount < 6) {
           for (let i = sliderData.totalCount; i < 7; i++) {
             this.sliderDetail.users.push({ isPlaceholder: true, role_name: 'talent', first_name: 'lorem', last_name: 'ipusam', meta: { profile_image_path: this.fallbackImage, date_of_birth: '04-01-2002' } });
           }
-          console.warn('placeholders ', this.sliderDetail.users);
+          detailTxt = 'PlaceHolders Added ';
+         
         } else {
-          console.warn('Slider is greater than 6');
+          detailTxt = ' ';
+          // console.warn('Slider is greater than 6');
         }
+        // console.warn('placeholders ', this.sliderDetail.users);
+        console.warn(detailTxt, this.sliderDetail.users);
         this.club_logo_path = this.sliderDetail.imagePath;
         this.pre_club_logo_path = this.sliderDetail.flagPath;
         this.advertisementData = res.data.advertisementData;
