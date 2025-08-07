e550771dc2b886fbda5debc9802c484fb893c733
Amrit <amritpalsharma.cts@gmail.com>
2025-08-07 15:01:09 +0530

    talk changes again

--- src/app/services/talkjs.service.ts
+++ src/app/services/talkjs.service.ts
@@ -22,7 +22,7 @@ export class TalkService {
   }
   async init(user: any): Promise<Talk.Session> {
 
-    let authToken = localStorage.getItem('authToken');
+    // let authToken = localStorage.getItem('authToken');
 
     // 🔄 Fetch both appId and signature from backend
     const res = await fetch('https://api.socceryou.ch/api/get-talk-signature?chatMode=' + this.chatMode, {
