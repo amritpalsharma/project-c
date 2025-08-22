0ca877139f072f842a67ac156cc5f9b1976df4e9
Amrit <amritpalsharma.cts@gmail.com>
2025-08-22 11:55:00 +0530

    live mode done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -189,11 +189,11 @@ export const environment = {
     'home', 'talent', 'clubs_and_scouts', 'about_us', 'pricing', 'news', 'contact', 'imprint', 'privacy_policy', 'terms_and_conditions', 'cookie_policy', 'faq', 'content_page'
   ],
   // apiUrl: 'https://apitest.socceryou.ch/api/',  
-  paymentMode: 'test',
-  apiUrl: 'https://apitest.socceryou.ch/api/',
-  apiBaseUrl: 'https://apitest.socceryou.ch/',
-  baseUrl:'https://apitest.socceryou.ch/uploads/',
-  url: 'https://apitest.socceryou.ch/',
+  paymentMode: 'live',
+  apiUrl: 'https://api.socceryou.ch/api/',
+  apiBaseUrl: 'https://api.socceryou.ch/',
+  baseUrl:'https://api.socceryou.ch/uploads/',
+  url: 'https://api.socceryou.ch/',
   socketUrl: 'https://alerts.socceryou.ch/',
   mailchimp: {
     apiUrl: 'https://us5.api.mailchimp.com/3.0/lists/7afbbb070a/members',
