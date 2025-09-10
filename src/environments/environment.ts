8e9407a193b2bd34df0d1942b23dc78daaecc3f9
Amrit <amritpalsharma.cts@gmail.com>
2025-09-10 18:43:35 +0530

    tiktok added to footer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -106,77 +106,6 @@ export const environment = {
     { name: 'Schweden', slug: 'se', id: 10, flag: 'Sweden-sweden.svg', locale: 'sv-SE' },
   ],
 
-
-  // colors: [
-  //   // Reds and Pinks
-  //   '#FF0000', // Red
-  //   '#FFCDD2', // Light Red
-  //   '#DC143C', // Crimson
-  //   '#FF4500', // Orange Red
-  //   '#FF69B4', // Hot Pink
-  //   '#FFC0CB', // Pink
-  //   '#FF1493', // Deep Pink
-  //   '#F8BBD0', // Light Pink
-
-  //   // Oranges and Yellows
-  //   '#FFA500', // Orange
-  //   '#FFE0B2', // Light Orange
-  //   '#FFD700', // Gold
-  //   '#FFECB3', // Light Amber
-  //   '#FFF9C4', // Light Yellow
-  //   '#F0E68C', // Khaki
-  //   '#FFDAB9', // Peach
-
-  //   // Greens
-  //   '#00FF00', // Lime Green
-  //   '#C8E6C9', // Light Green
-  //   '#008000', // Dark Green
-  //   '#ADFF2F', // Green Yellow
-  //   '#7CFC00', // Lawn Green
-  //   '#3CB371', // Medium Sea Green
-  //   '#B2DFDB', // Light Teal
-
-  //   // Blues and Cyans
-  //   '#0000FF', // Blue
-  //   '#BBDEFB', // Light Blue
-  //   '#1E90FF', // Dodger Blue
-  //   '#4169E1', // Royal Blue
-  //   '#00FFFF', // Cyan
-  //   '#B2EBF2', // Light Cyan
-  //   '#40E0D0', // Turquoise
-  //   '#7FFFD4', // Aquamarine
-
-  //   // Purples and Violets
-  //   '#800080', // Purple
-  //   '#E1BEE7', // Light Purple
-  //   '#8A2BE2', // Blue Violet
-  //   '#9370DB', // Medium Purple
-  //   '#DA70D6', // Orchid
-  //   '#EE82EE', // Violet
-  //   '#DDA0DD', // Plum
-
-  //   // Browns and Neutrals
-  //   '#A52A2A', // Brown
-  //   '#D7CCC8', // Light Brown
-  //   '#CD853F', // Peru
-  //   '#D2691E', // Chocolate
-  //   '#F5F5F5', // Light Grey
-  //   '#808080', // Gray
-  //   '#C0C0C0', // Silver
-  //   '#FFFFFF', // White
-  //   '#000000', // Black
-
-  //   // Additional Colors
-  //   '#4B0082', // Indigo
-  //   '#6A5ACD', // Slate Blue
-  //   '#FF00FF', // Magenta
-  //   '#E6E6FA', // Lavender
-  //   '#FA8072', // Salmon
-  //   '#CD5C5C', // Indian Red
-  //   '#008080', // Teal
-  //   '#000080', // Navy
-  //   '#808000', // Olive
-  // ],
   colors: [
     // Reds and Pinks
     '#FF0000', // Red
@@ -194,7 +123,7 @@ export const environment = {
   apiBaseUrl: 'https://apitest.socceryou.ch/',
   baseUrl: 'https://apitest.socceryou.ch/uploads/',
   url: 'https://apitest.socceryou.ch/',
-  socketUrl: 'https://alerts.socceryou.ch/',
+  socketUrl: 'https://alerts.socceryou.ch/', 
   mailchimp: { 
     apiUrl: 'https://us5.api.mailchimp.com/3.0/lists/7afbbb070a/members',
     apiKey: '62a0ffa8d6e9c3d5ed7c8a09e9111b41-us17',  // Correct API key
